5acdcf663ffdd4c572152c26bd0bb0db6403b79e
AlanNunez30 <alan_589_2@fpuna.edu.py>
2026-02-03 18:58:39 -0300

    Creé la tabla Negocios

--- app/models/database.py
+++ app/models/database.py
@@ -1,3 +1,28 @@
 import sqlite3
 import pandas as pd
 import os
+
+DB_NAME = "negocios.db"
+
+def conectar():
+    return sqlite3.connect(DB_NAME)
+
+def db_init():
+    conn = conectar()
+    cursor = conn.cursor()
+
+ # Tabla Negocios
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS negocios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT,
+        direccion TEXT,
+        tiene_web INTEGER,
+        categoria TEXT,
+        rating REAL,
+        telefono TEXT,
+        lat REAL,
+        lon REAL,
+        status TEXT DEFAULT 'Pendiente'
+    )
+    """)
