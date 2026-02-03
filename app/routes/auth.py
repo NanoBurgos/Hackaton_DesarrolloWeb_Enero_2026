29256e07a6cbb71905e2fb20597cec83659aa7b2
David <davillar.dv.321@gmail.com>
2026-02-03 19:22:36 -0300

    Agregue endpoint de logout

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -18,3 +18,10 @@ def login():
             return redirect(url_for('main.index'))
 
     return render_template('login.html')
+
+# Logout de usuario
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
